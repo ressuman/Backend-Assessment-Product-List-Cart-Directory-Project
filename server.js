c1a4cc11cb4ad60fcdc579f6198804a81ff8773a
Richard Essuman <ressuman001@gmail.com>
2024-11-19 22:34:37 +0000

    Add Swagger and OpenAPI support with documentation setup in server.js and update dependencies in package.json

--- server.js
+++ server.js
@@ -8,6 +8,10 @@ import xss from "xss-clean";
 import hpp from "hpp";
 import rateLimit from "express-rate-limit";
 import dotenv from "dotenv";
+// import swaggerUi from "swagger-ui-express";
+// import swaggerJsdoc from "swagger-jsdoc";
+// import postmanToOpenApi from "postman-to-openapi";
+// import swaggerJson from "./openapi.json" assert { type: "json" };
 
 dotenv.config();
 
@@ -62,6 +66,54 @@ app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/user", userRoutes);
 app.use("/api/v1/products", productRoutes);
 app.use("/api/v1/cart", cartRoutes);
+
+// Swagger Documentation
+// const swaggerSpec = swaggerJsdoc({
+//   definition: {
+//     openapi: "3.0.0",
+//     info: {
+//       title: "Product List and Shopping Cart API's",
+//       description:
+//         "An backend API design that supports CRUD operations of product lists and shopping cart functionalities.",
+//     },
+//     servers: [
+//       {
+//         url: "http://localhost:3050/api/v1",
+//       },
+//     ],
+//   },
+//   apis: ["./routes/*.js"], // Specify the paths to your route files
+// });
+
+// app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+// app.get("/swagger-json", (req, res) => {
+//   res.setHeader("Content-Type", "application/json");
+//   res.send(swaggerJson);
+// });
+
+// // Generate OpenAPI YAML from Postman Collection
+// app.get("/generate-yml", async (req, res) => {
+//   const postmanCollection =
+//     "./Product List and Shopping Cart API's.postman_collection.json"; // Path to your Postman collection file
+//   const outputFile = "collection.yml"; // Desired YAML output path
+
+//   try {
+//     const result = await postmanToOpenApi(postmanCollection, outputFile, {
+//       defaultTag: "General",
+//     });
+//     console.log(`OpenAPI specs generated at: ${result}`);
+//     res.status(200).send(`OpenAPI specs generated at: ${result}`);
+//   } catch (err) {
+//     console.error(err);
+//     res.status(500).send("Failed to generate OpenAPI YAML");
+//   }
+// });
+
+// // Default Route
+// app.get("/", (req, res) => {
+//   res.status(200).send("API is running...");
+// });
 
 // Error handler for unsupported routes
 app.use((req, res, next) => {
