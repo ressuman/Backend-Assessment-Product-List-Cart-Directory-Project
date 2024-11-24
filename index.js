6f49c13a065609434e431f09be14152c133aa219
Richard Essuman <ressuman001@gmail.com>
2024-11-24 23:24:27 +0000

    Comment out Swagger UI and OpenAPI documentation imports in index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import fs from "fs/promises";
-import swaggerUi from "swagger-ui-express";
+//import fs from "fs/promises";
+//import swaggerUi from "swagger-ui-express";
 import dotenv from "dotenv";
 dotenv.config();
 
@@ -7,14 +7,14 @@ import dbconfig from "./config/dBConfig.js";
 
 import server from "./server.js";
 
-const swaggerDocs = JSON.parse(
-  await fs.readFile("./docs/openapi.json", "utf-8")
-);
+// const swaggerDocs = JSON.parse(
+//   await fs.readFile("./docs/openapi.json", "utf-8")
+// );
 
 const PORT = process.env.PORT_NUMBER || 3320;
 const HOST = process.env.PORT_HOST || "localhost";
 
-server.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+//server.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 server.listen(PORT, HOST, () => {
   console.log(
